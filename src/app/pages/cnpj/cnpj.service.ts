46900091ab36d2dacfbd946efb0af61698a35c32
Thiago Lopes <thiagoolopezz@gmail.com>
2022-05-23 21:14:36 -0300

    Update cnpj.service.ts

--- src/app/pages/cnpj/cnpj.service.ts
+++ src/app/pages/cnpj/cnpj.service.ts
@@ -6,17 +6,11 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CnpjService {
 
-  apiUrl: string
+  apiUrl: string;
 
   constructor(private http: HttpClient) {
 
     this.apiUrl = `https://brasilapi.com.br/api/cnpj/v1`;
    }
-
-    obterDados(cnpj: any): Promise<any>{
-      return this.http.get(`${this.apiUrl}/${cnpj}`)
-      .toPromise()
-      .then( response => response);
-    }
 
 }
