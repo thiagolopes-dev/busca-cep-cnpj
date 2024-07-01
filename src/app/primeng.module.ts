bf4bd100b8512608330ab91285f789b7f4e2f3ef
Thiago Lopes <thiagoolopezz@gmail.com>
2024-07-01 14:08:14 -0300

    add developer

--- src/app/primeng.module.ts
+++ src/app/primeng.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
+import { ChipModule } from 'primeng/chip';
 import { DialogModule } from 'primeng/dialog';
 import { InputMaskModule } from 'primeng/inputmask';
 import { InputTextModule } from 'primeng/inputtext';
 import { PanelModule } from 'primeng/panel';
 import { ToastModule } from 'primeng/toast';
 import { TooltipModule } from 'primeng/tooltip';
+
 @NgModule({
 
   exports: [
@@ -17,7 +19,8 @@ import { TooltipModule } from 'primeng/tooltip';
       InputMaskModule,
       ToastModule,
       PanelModule,
-      DialogModule
+      DialogModule,
+      ChipModule
   ]
 })
 export class PrimengModule { }
